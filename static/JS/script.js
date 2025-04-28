4d327b14918cef5e75a236ebc023c882a6fc2b1d
rifqiadrianto007 <232410103042@mail.unej.ac.id>
2025-04-28 18:51:34 +0700

    command erase

--- static/JS/script.js
+++ static/JS/script.js
@@ -1,15 +1,12 @@
+// Scroll card
 document.addEventListener('DOMContentLoaded', function() {
-    // Ambil elemen container dan tombol
     const gameCardsContainer = document.querySelector('.game-cards-container');
     const prevButton = document.querySelector('.carousel-control.prev');
     const nextButton = document.querySelector('.carousel-control.next');
-    
-    // Hitung jumlah pergeseran (lebar kartu + gap)
-    const cardWidth = 220; // Lebar setiap kartu
-    const cardGap = 20;    // Jarak antar kartu
+    const cardWidth = 220;
+    const cardGap = 20; 
     const scrollAmount = cardWidth + cardGap;
-    
-    // Tambahkan event listener untuk tombol sebelumnya
+
     prevButton.addEventListener('click', function() {
         gameCardsContainer.scrollBy({
             left: -scrollAmount,
@@ -17,7 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
     
-    // Tambahkan event listener untuk tombol selanjutnya
     nextButton.addEventListener('click', function() {
         gameCardsContainer.scrollBy({
             left: scrollAmount,
@@ -26,14 +22,10 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
+// Back to top
 document.addEventListener('DOMContentLoaded', function() {
-    // Mendapatkan tombol "Back to Top"
     const backToTopButton = document.querySelector('.btn-back-to-top');
-    
-    // Menambahkan event listener untuk klik pada tombol
     backToTopButton.addEventListener('click', function() {
-        // Menggulir ke bagian paling atas halaman dengan efek smooth
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
